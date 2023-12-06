10b1366326dfcbd6b8e1b646dee1f26b24ed5219
mershark <mersharktakyi@gmail.com>
2023-12-06 20:14:54 +0000

    Create App

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+greetings = [
+  "Hello, how are you today?",
+  "Greetings and salutations!",
+  "Welcome to our platform!",
+  "Good day! Enjoy your stay.",
+  "Hi there! It's a pleasure to meet you."
+]
+
+greetings.each do |greeting|
+  Message.create(content: greeting)
+end
+
+puts "Seed data successfully added!"
